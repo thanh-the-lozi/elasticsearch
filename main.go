bee5a3b61188a779d4a3f72873adf73575b75f02
nguyen the <nguyenthe0202.1997@gmail.com>
2020-05-27 15:49:03 +0700

    something

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	// Context object for Do() methods
 	"context" // Log errors and quit
 	// Get object methods and attributes
 	"fmt"  // Format and print cluster data
@@ -14,6 +13,7 @@ import (
 var (
 	client *elastic.Client
 	ctx    context.Context
+	err    error
 )
 
 const (
@@ -39,7 +39,31 @@ const (
 )
 
 func main() {
-	var err error
+	client, err = NewClient()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("client info:", client)
+
+	ctx = context.Background()
+
+	DeleteAllIndexes()
+	// CreateIndex("some_index")
+	// IndexExists("s")
+	// IndexDocument("index2", "1", "film", mapping)
+	// GetDocument("students", "1", "", mapping)
+	// ListIndexNames()
+
+	// // Xoá một index
+	// DeleteIndex("index2")
+	// ListIndexNames()
+}
+
+//=============================//
+
+// Tạo client
+func NewClient() (*elastic.Client, error) {
 	// khai báo một số option của client
 	options := []elastic.ClientOptionFunc{
 		elastic.SetSniff(true),
@@ -48,22 +72,78 @@ func main() {
 	}
 
 	// tạo client với các option trên
-	client, err = elastic.NewClient(options...)
+	return elastic.NewClient(options...)
+}
+
+// Tạo index
+func CreateIndex(index string) {
+	createdIndex, err := client.CreateIndex(index).BodyString(mapping).Do(ctx)
 	if err != nil {
-		panic(err)
-	} else {
-		fmt.Println("client:", client)
+		fmt.Println(err)
 	}
-	ctx = context.Background()
+	fmt.Println("create: ", createdIndex)
+}
 
-	DeleteAllIndexes()
-	CreateIndex("some_index")
-	IndexExists("some_index")
-	IndexDocument("index2", "1", "film", mapping)
-	GetDocument("index2", "1", "film", mapping)
-	ListIndexNames()
+// Kiểm tra index tồn tại chưa
+func IndexExists(index string) {
+	exist, err := client.IndexExists(index).Do(ctx)
+	if err != nil {
+		fmt.Println("exist:", err)
+	} else {
+		fmt.Println("exist:", exist)
+	}
+}
 
-	// Xoá một index
-	DeleteIndex("index2")
-	ListIndexNames()
+// Đánh index cho document
+func IndexDocument(index, id, _type string, document interface{}) {
+	put1, _ := client.Index().
+		Index(index).
+		BodyJson(document).
+		Type(_type).
+		Id(id).
+		Do(ctx)
+
+	fmt.Println("index: ", put1.Id, put1.Version, put1.Index, put1.Type)
+}
+
+// Lấy document từ index
+func GetDocument(index, id, _type string, document interface{}) {
+	res, err := client.Get().Index(index).Id(id).Type(_type).Do(ctx)
+	if err != nil {
+		fmt.Println("get: ", err)
+	} else {
+		if res.Found {
+			fmt.Println("get res: ", res.Id, res.Version, res.Index, res.Type)
+		} else {
+			fmt.Println("not found")
+		}
+	}
+}
+
+// Xóa index
+func DeleteIndex(index string) {
+	_, err := client.DeleteIndex(index).Do(ctx)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("deleted index: ", index)
+}
+
+// Lấy danh sách tên index
+func ListIndexNames() {
+	names, _ := client.IndexNames()
+	for _, name := range names {
+		fmt.Println(name)
+	}
+}
+
+// Xóa tất cả index
+func DeleteAllIndexes() {
+	names, _ := client.IndexNames()
+	for _, name := range names {
+		DeleteIndex(name)
+	}
 }
